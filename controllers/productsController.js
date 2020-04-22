3c1276d2707aca95e8423dcebf90dab71b8e43f6
peteco83 <peteco1983@outlook.com>
2020-04-22 13:59:42 +0200

    Add the products db

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,26 +1,33 @@
-
-const products = []
+const db = require("../models/db")
+const shortid = require('shortid');
 
 exports.getProducts = (req,res,next) => {
-    res.json({products: products})
+    let products = db.get("products").value()
+    res.json({success: true, products: products})
     
 }
 
+exports.getProduct = (req,res,next) => {
+    const {id} = req.params
+    let product = db.get("products").find({id}).value()
+    res.json({success: true, product: product})
+}
+
 exports.postProduct = (req,res,next) => {
-    console.log(req.body)
-    if(!products.includes(req.body)){
-        products.push(...req.body)
-    }
-    res.json({products})
+    db.get("products").push(req.body).last().assign({id:shortid.generate()}).write()
+    res.json({success: true, product: req.body})
 }
 
 exports.putProduct = (req, res, next) => {
-    console.log(req.params.id)
-    console.log(req.body)
-    res.send("put request received on /products/:id")
+    const {id} = req.params
+    const product = req.body
+    product.id = shortid.generate()
+    db.get("products").find({id}).assign(product).write()
+    res.json({success:true, product: product})
 }
 
 exports.deleteProduct = (req, res, next) => {
-    console.log(req.params.id)
-    res.send("received delete request on /products/:id")
+   const {id} = req.params
+   const product = db.get("products").remove({id}).write()
+   res.json({success: true, product: product})
 }
