e91148ef3fdffa0bf21ee7204eaff0d5c72c6869
peteco83 <peteco1983@outlook.com>
2020-04-23 14:38:59 +0200

    add schema for clients

---
+++ models/clientsSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const {Schema} = mongoose;
+
+const clientSchema = new Schema({
+    firstName: {type: String, required: true},
+    // si no pone required el usuario se lo puede saltar
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+},
+
+{    
+    toObject: {
+        virtuals: true
+    },
+    toJSON: {
+        virtuals: true
+    }
+
+    // we are not going to use the virtuals
+})
+
+UserSchema.virtual("fullName").get(function(){ 
+    return `${this.firstName} ${this.lastName}`
+})
+
+module.exports = mongoose.model("Client", clientSchema)
