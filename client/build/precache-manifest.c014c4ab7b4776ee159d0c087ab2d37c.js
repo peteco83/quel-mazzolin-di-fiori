8e336b6fca6c5799a58e5cc80bfdc6b8abff5db3
peteco83 <peteco1983@outlook.com>
2020-05-13 09:30:20 +0200

    Added build order

---
+++ client/build/precache-manifest.c014c4ab7b4776ee159d0c087ab2d37c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71bdc6c59d79da5cb1d063f8ef770a40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ca3dbf722c3bb058011",
+    "url": "./static/css/main.780476ea.chunk.css"
+  },
+  {
+    "revision": "1fd488642f20bf9786ea",
+    "url": "./static/js/2.979af1df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.979af1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca3dbf722c3bb058011",
+    "url": "./static/js/main.bf72b949.chunk.js"
+  },
+  {
+    "revision": "74c3360647771a2cb023",
+    "url": "./static/js/runtime-main.8f7cb1c9.js"
+  },
+  {
+    "revision": "95e03e48291f5e1abbbe3c40141593f0",
+    "url": "./static/media/antipasti.95e03e48.jpg"
+  },
+  {
+    "revision": "3744b6d6ff9ba8480a92c94b803c2f09",
+    "url": "./static/media/focaccia.3744b6d6.jpg"
+  },
+  {
+    "revision": "5c056c7e603a7a8e424cd2d5e02167c5",
+    "url": "./static/media/pasta.5c056c7e.jpg"
+  },
+  {
+    "revision": "15d69df412cf35b1f9940f8950234f38",
+    "url": "./static/media/pizza-3007395_1920.15d69df4.jpg"
+  },
+  {
+    "revision": "300d72e24d32a630f414ee92ca4db410",
+    "url": "./static/media/pizza.300d72e2.jpg"
+  },
+  {
+    "revision": "d84bb0ab4fae90b6de46ed1903a74387",
+    "url": "./static/media/tiramisu.d84bb0ab.jpg"
+  }
+]);
